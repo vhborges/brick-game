ada17f123158f142221143b5df5f31f2fd2cdd9d
Juliano Fantozzi <juufantozzi@hotmail.com>
2018-06-29 19:50:22 -0300

    terminando move barra com 3 pos, store com problema

--- BrickGame.asm
+++ BrickGame.asm
@@ -17,8 +17,6 @@ tamBloco: var #1
 main:
   call InicializaVariaveis
   call ImprimeTela
-  loadn r0, #0 ; contador para os mods
-  loadn r2, #0 ; para verificar o resultado do módulo
   jmp Loop
 
 InicializaVariaveis:
@@ -41,74 +39,105 @@ InicializaVariaveis:
 
   loadn r0, #1100 ; posicao inicial da bola
   store posBola, r0
-
-;---- escrevendo o primeiro vetor ----
-
+  
+;---- escrevendo no primeiro vetor ----
+  ;--------posicao do primeiro char de cada barra. (comprimento de 5 chars por barra) 
   loadn r0, #80
-  store posBlocos1, r0
-
+  loadn r1, #posBlocos1
+  storei r1, r0 
+  
   loadn r0, #90
-  store posBlocos1 + 1, r0
-
+  inc r1
+  storei r1, r0
+  
   loadn r0, #100
-  store posBlocos1 + 2, r0
-
+  inc r1
+  storei r1, r0
+  
   loadn r0, #110
-  store posBlocos1 + 3, r0
-
-  load r0, #1
-  store posBlocos1 + 4, r0
-  store posBlocos1 + 5, r0
-  store posBlocos1 + 6, r0
-  store posBlocos1 + 7, r0
-
-;---- escrevendo o segundo vetor ----
-
+  inc r1
+  storei r1, r0
+  ;--------#posicao
+  ;--------validade
+  loadn r0, #1
+  inc r1
+  storei r1, r0
+  inc r1
+  storei r1, r0
+  inc r1
+  storei r1, r0
+  inc r1
+  storei r1, r0
+  ;--------validade
+  
+;---- escrevendo no segundo vetor ----
+  ;--------#posicao
   loadn r0, #125
-  store posBlocos2, r0
-
+  loadn r1, #posBlocos2
+  storei r1, r0
+  
   loadn r0, #135
-  store posBlocos2 + 1, r0
-
+  inc r1
+  storei r1, r0
+  
   loadn r0, #145
-  store posBlocos2 + 2, r0
-
+  inc r1
+  storei r1, r0
+  
   loadn r0, #155
-  store posBlocos2 + 3, r0
-
-  load r0, #1
-  store posBlocos2 + 4, r0
-  store posBlocos2 + 5, r0
-  store posBlocos2 + 6, r0
-  store posBlocos2 + 7, r0
-
-;---- escrevendo o terceiro vetor ----
-
+  inc r1
+  storei r1, r0
+  ;--------#posicao
+  ;--------validade
+  loadn r0, #1
+  inc r1
+  storei r1, r0
+  inc r1
+  storei r1, r0
+  inc r1
+  storei r1, r0
+  inc r1
+  storei r1, r0
+  ;--------validade
+  
+;---- escrevendo no terceiro vetor ----
+  ;--------#posicao
   loadn r0, #160
-  store posBlocos3, r0
-
+  loadn r1, #posBlocos3
+  storei r1, r0
+  
   loadn r0, #170
-  store posBlocos3 + 1, r0
-
+  inc r1
+  storei r1, r0
+  
   loadn r0, #180
-  store posBlocos3 + 2, r0
-
+  inc r1
+  storei r1, r0
+  
   loadn r0, #190
-  store posBlocos3 + 3, r0
-
-  load r0, #1
-  store posBlocos3 + 4, r0
-  store posBlocos3 + 5, r0
-  store posBlocos3 + 6, r0
-  store posBlocos3 + 7, r0
-
+  inc r1
+  storei r1, r0
+  ;--------#posicao
+  ;--------validade
+  loadn r0, #1
+  inc r1
+  storei r1, r0
+  inc r1
+  storei r1, r0
+  inc r1
+  storei r1, r0
+  inc r1
+  storei r1, r0
+  ;--------validade
+  
+  
   pop r0
   rts
 
 ImprimeTela:
   call ImprimeBarra
   call ImprimeBlocos
-  call ImprimeBola
+  ;call ImprimeBola
   rts
 
 ImprimeBarra:
@@ -131,23 +160,93 @@ ImprimeBarra:
   pop r2
   pop r1
   pop r0
-
+  
   rts
 
+
 ImprimeBlocos:
-;---- imprimindo o primeiro vetor ----
   push r0
-  push r1
-  push r2
-
-  load r0, posBlocos1 ; primeira posição do primeiro bloco
   
-  load r1, posBlocos1 + 4 ; validade do bloco
+  ;imprime blocos do primeiro vetor
+  loadn r0, #posBlocos1
+  
+  loadi r7 ,r0 
+  call ImprimeBlocoLoop ; imprime primeiro bloco
+  
+  inc r0
+  loadi r7, r0
+  call ImprimeBlocoLoop ; imprime segundo bloco
+  
+  inc r0
+  loadi r7, r0
+  call ImprimeBlocoLoop ; imprime terceiro bloco
+  
+  inc r0
+  loadi r7, r0
+  call ImprimeBlocoLoop ; imprime quarto bloco
+  ;---------------------------------------------
+  
+  ;imprime blocos do segundo vetor
+  loadn r0, #posBlocos2
+  
+  loadi r7 ,r0 
+  call ImprimeBlocoLoop ; imprime primeiro bloco
+  
+  inc r0
+  loadi r7, r0
+  call ImprimeBlocoLoop ; imprime segundo bloco
+  
+  inc r0
+  loadi r7, r0
+  call ImprimeBlocoLoop ; imprime terceiro bloco
+  
+  inc r0
+  loadi r7, r0
+  call ImprimeBlocoLoop ; imprime quarto bloco
+  
+  ;---------------------------------------------
+  
+  ;imprime blocos do terceiro vetor
+  loadn r0, #posBlocos3
+  
+  loadi r7 ,r0 
+  call ImprimeBlocoLoop ; imprime primeiro bloco
+  
+  inc r0
+  loadi r7, r0
+  call ImprimeBlocoLoop ; imprime segundo bloco
+  
+  inc r0
+  loadi r7, r0
+  call ImprimeBlocoLoop ; imprime terceiro bloco
+  
+  inc r0
+  loadi r7, r0
+  call ImprimeBlocoLoop ; imprime quarto bloco
+  
+  pop r0
 
-  load r2, #1
 
-  cmp r1, r2
-  jeq 
+ImprimeBlocoLoop: ; r7 = posicao inicial de print
+  push r0
+  
+  loadn r0, #'='
+  outchar r0, r7
+  inc r7
+  outchar r0, r7
+  inc r7
+  outchar r0, r7
+  inc r7
+  outchar r0, r7
+  inc r7
+  outchar r0, r7
+  inc r7
+  
+  pop r0
+  
+  rts
+
+
 
 ImprimeBola:
 
@@ -158,7 +257,7 @@ Loop:
   loadn r1, #2
   mod r1, r0, r1
   cmp r1, r2      ; verifica se o resultado do mod deu zero
-  ceq MoveBola ; chama rotina de movimentacao da bolinha
+  ;ceq MoveBola ; chama rotina de movimentacao da bolinha
 
   call Delay
   inc r0          ; incrementa o contador
@@ -166,28 +265,50 @@ Loop:
 
 MoveBarra:
   inchar r1                     ; le Teclado para controlar a barra
+  ;loadn r1, #'d'
   loadn r2, #'a'
   cmp r1, r2
-  jeq MoveBarra_RecalculaPos_A  ;chama funcao para mover para a direita se apertar A
+  ceq MoveBarra_RecalculaPos_A  ;chama funcao para mover para a direita se apertar A
 
   loadn r2, #'d'
   cmp r1, r2
-  jeq MoveBarra_RecalculaPos_D  ;chama funcao para mover para a esquerda se apertar D
+  ceq MoveBarra_RecalculaPos_D  ;chama funcao para mover para a esquerda se apertar D
 
   rts
 
 MoveBarra_RecalculaPos_A:
-  push r0
-  push r1
-  push r2
-
+  push r0 ; barra
+  push r1 ; barra - 1
+  push r2 ; barra + 1
+  push r3 ; char '_'
+  push r4 ; char ' '
+  
+  
   load r0, posBarra
-  load r1, charBarra
-  loadn r2, #' '
+  
+  mov r1, r0
+  inc r1
+  
+  mov r2, r0
+  dec r2
+  
+  load r3, charBarra
+  loadn r4, #' '
 
-  outchar r2, r0     ; limpa a posição anterior da barra
-  dec r0             ; decrementa a posição da barra (move para esquerda)
-  outchar r1, r0     ; imprime a nova posição
+  ; limpa a posição anterior da barra
+  outchar r4, r0
+  outchar r4, r1
+  outchar r4, r2
+  ; decrementa a posição da barra (move para esquerda)
+  dec r0  
+  dec r1  
+  dec r2  
+  ; imprime a nova posição
+  outchar r3, r0
+  outchar r3, r1
+  outchar r3, r2
+  
+  
   store posBarra, r0 ; armazena a nova posição na memória
 
   pop r2
@@ -197,18 +318,37 @@ MoveBarra_RecalculaPos_A:
   rts
 
 MoveBarra_RecalculaPos_D:
-  push r0
-  push r1
-  push r2
-
+  push r0 ; barra
+  push r1 ; barra - 1
+  push r2 ; barra + 1
+  push r3 ; char '_'
+  push r4 ; char ' '
+  breakp
   load r0, posBarra
-  load r1, charBarra
-  loadn r2, #' '
+  
+  mov r1, r0
+  inc r1
+  
+  mov r2, r0
+  dec r2
+  
+  load r3, charBarra
+  loadn r4, #' '
 
-  outchar r2, r0      ; limpa a posição anterior da barra
-  inc r0              ; incrementa a posição da barra (move para direita)
-  outchar r1, r0      ; imprime a nova posição
-  store posBarra, r0  ; armazena a nova posição na memória
+  ; limpa a posição anterior da barra
+  outchar r4, r0
+  outchar r4, r1
+  outchar r4, r2
+  ; incrementa a posição da barra (move para esquerda)
+  inc r0  
+  inc r1  
+  inc r2  
+  ; imprime a nova posição
+  outchar r3, r0
+  outchar r3, r1
+  outchar r3, r2
+  
+  store posBarra, r0 ; armazena a nova posição na memória
 
   pop r2
   pop r1
