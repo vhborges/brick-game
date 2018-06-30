0c25d659512b76f2eda8c1e64b0024186d12760c
vhborges <viniciusborges22@gmail.com>
2018-06-30 15:29:50 -0300

    bolinha percorrendo o cenário

--- BrickGame.asm
+++ BrickGame.asm
@@ -11,14 +11,16 @@ charBarra: var #1
 charBola: var #1
 charBloco: var #1
 tamBloco: var #1
+incBola: var #1
+incBolaNegativo: var #1
 
 ;---- Inicio do Programa Principal -----
 
 main:
   call InicializaVariaveis
   call ImprimeTela
-  ;loadn r0, #0 ; contador para os mods
-  ;loadn r2, #0 ; para verificar o resultado do módulo
+  loadn r0, #0 ; contador para os mods
+  loadn r2, #0 ; para verificar o resultado do módulo
   jmp Loop
 
 InicializaVariaveis:
@@ -41,6 +43,12 @@ InicializaVariaveis:
 
   loadn r0, #1100 ; posicao inicial da bola
   store posBola, r0
+
+  loadn r0, #41   ; incremento inicial da bola
+  store incBola, r0
+
+  loadn r0, #1    ; o numero do incBola é negativo
+  store incBolaNegativo, r0
   
 ;---- escrevendo no primeiro vetor ----
   ;--------posicao do primeiro char de cada barra. (comprimento de 5 chars por barra) 
@@ -138,7 +146,7 @@ InicializaVariaveis:
 ImprimeTela:
   call ImprimeBarra
   call ImprimeBlocos
-  ;call ImprimeBola
+  call ImprimeBola
   rts
 
 ImprimeBarra:
@@ -246,18 +254,28 @@ ImprimeBlocoLoop: ; r7 = posicao inicial de print
   rts
 
 ImprimeBola:
+  push r0
+  push r1
 
+  load r0, charBola
+  load r1, posBola
+  outchar r0, r1
+
+  pop r1
+  pop r0
+
+  rts
 
 Loop:
   call MoveBarra  ; maior velocidade do jogo
 
-  ;loadn r1, #2
-  ;mod r1, r0, r1
-  ;cmp r1, r2      ; verifica se o resultado do mod deu zero
-  ;ceq MoveBola ; chama rotina de movimentacao da bolinha
+  loadn r1, #2
+  mod r1, r0, r1
+  cmp r1, r2      ; verifica se o resultado do mod deu zero
+  ceq MoveBola ; chama rotina de movimentacao da bolinha
 
   call Delay
-  ;inc r0          ; incrementa o contador
+  inc r0          ; incrementa o contador
   jmp Loop
 
 MoveBarra:
@@ -361,17 +379,48 @@ MoveBarra_RecalculaPos_D:
   
   store posBarra, r0 ; armazena a nova posição na memória
 
-Retorna:
-  pop r5
-  pop r4
-  pop r3
-  pop r2
-  pop r1
-  pop r0
-  rts
+  Retorna:
+    pop r5
+    pop r4
+    pop r3
+    pop r2
+    pop r1
+    pop r0
+    rts
 
 MoveBola:
+  push r0
+  push r1
+  push r2
+  push r3
+  push r4
 
+  loadn r0, #' '    ;
+  load r1, posBola  ; Apaga a posição atual da bola
+  outchar r0, r1    ;
+
+  load r0, charBola         ;
+  load r2, incBola          ; obtém o valor de incremento da bola
+  load r3, incBolaNegativo  ;
+
+  loadn r4, #1    ;
+  cmp r3, r4      ; verifica se o incremento é negativo
+  jeq Negativo    ;
+  add r1, r1, r2  ; se não for, adiciona o incremento da posição atual
+  jmp Pula        ; e pula a instrução de sub
+  Negativo:
+    sub r1, r1, r2  ; se for, subtrai o incremento da posição atual
+  Pula:
+    outchar r0, r1  ; escreve a nova posição da bola
+
+    store posBola, r1 ; armazena a nova posição na memória
+
+    pop r4
+    pop r3
+    pop r2
+    pop r1
+    pop r0
+    rts
 
 Delay:
   push r0
